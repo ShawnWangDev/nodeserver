faf4c829ab10c558cbb327bd769f007e4f7e8407
wangsc801 <wangsc801@163.com>
2022-03-04 07:17:12 +0800

    tune

--- routes/administrative_test.js
+++ routes/administrative_test.js
@@ -2,6 +2,7 @@ var express = require('express');
 var fs=require('fs')
 const path=require('path');
 const math_36methods_data=require('../json_data/math_36methods.json')
+const police_law_json=require('../json_data/poilce_law.json')
 var router = express.Router();
 /* GET home page. */
 router.get('/home', function(req, res, next) {
@@ -13,3 +14,10 @@ router.get('/math_36methods',function(req,res,next){
   res.render('administrative_test/math_36methods', { title: 'study',data:math_36methods_data});
 })
 module.exports = router;
+
+router.get('/json',function(req,res,next){
+  var data=police_law_json
+  for(let i in data){
+    console.log(i.title)
+  }
+})
